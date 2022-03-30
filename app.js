2f270b871a055da8c7a632a78e0d130baebdba55
Augie Bren <Augie>
2022-03-29 19:49:02 -0500

    done with bonus

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 class Casino {
-    // Write code here
+    constructor(name, isFakeCoin, timesPlayed) {
+        this.name = name
+        this.timesPlayed = 0
+        this.isFakeCoin = isFakeCoin
+    }
+
+    playGame(betAmount) {
+        if (Math.random() <= .5 || this.isFakeCoin) {
+            console.log(this.name + ` wins!`)
+        } else {
+            this.timesPlayed++
+            console.log(`Player Wins! ${betAmount * this.timesPlayed}`)
+        }
+    }
+
+    rollDie(d) {
+        let number = Math.floor(Math.random() *d + 1)
+        console.log(`you rolled a ${number}`)
+    }
 };
 
 // TESTS
@@ -11,20 +29,19 @@ myCasino.playGame(25);
 myCasino.playGame(35);
 
 // BONUS TESTS
-/*
+
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
+
 
 // Extra BONUS TESTS
-/*
+
 const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
 console.log(myExtraBonusCasino);
 myExtraBonusCasino.rollDie(6);
 myExtraBonusCasino.rollDie(20);
 myExtraBonusCasino.rollDie(100);
-*/
