0bdb06720c17f9f4b395b8abb1213b60310fa08a
Augie Bren <Augie>
2022-03-30 00:16:07 -0500

    interesting

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 class Casino {
-    constructor(name, isFakeCoin, timesPlayed) {
+    constructor(name, isFakeCoin) {
         this.name = name
         this.timesPlayed = 0
         this.isFakeCoin = isFakeCoin
@@ -14,26 +14,38 @@ class Casino {
         }
     }
 
+    betAmount() {
+        if (this.betAmount > 0) {
+            this.playGame(this.betAmount * this.timesPlayed)
+        } else {
+            console.log(`you've busted ${this.timesPlayed}`)
+        }
+    }
+
     rollDie(d) {
         let number = Math.floor(Math.random() *d + 1)
         console.log(`you rolled a ${number}`)
     }
 };
 
-const augieCasino = new Casino("AugieDog", false);
+const augieCasino = new Casino("AugiesAces", false);
 console.log(augieCasino);
 augieCasino.playGame(5);
 augieCasino.playGame(10);
 augieCasino.playGame(25);
-augieCasino.rollDie(22)
+augieCasino.rollDie(22);
+augieCasino.betAmount(500);
+augieCasino.betAmount(10000)
 
-// TESTS
+// // TESTS
 const myCasino = new Casino("HackerU Casino");
 console.log(myCasino);
 myCasino.playGame(5);
 myCasino.playGame(15);
 myCasino.playGame(25);
 myCasino.playGame(35);
+myCasino.rollDie(10);
+myCasino.betAmount(10);
 
 // BONUS TESTS
 
@@ -43,6 +55,8 @@ myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
+myBonusCasino.rollDie(12);
+myBonusCasino.betAmount(800);
 
 
 // Extra BONUS TESTS
